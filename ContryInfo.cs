b0df346b21bf38ca509af8550f95ef273e0fb968
sYgcode <52917249+sYgcode@users.noreply.github.com>
2020-08-25 15:23:02 +0300

    Add files via upload

---
+++ ContryInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class ContryInfo : MonoBehaviour
+{
+    // lists to divide the gameobjects to choose who owns them
+    public static List<GameObject> myCountries = new List<GameObject>();
+    public static List<GameObject> AICountries = new List<GameObject>();
+
+    // array of game objects(the countries) that are in the game which i added in the inspector (as of now there are 5)
+    public GameObject[] Countries;
+
+    void Awake()
+    {
+        // a foreach loop that is meant to go through each country in the array and assign it randomly
+        foreach (GameObject Country in Countries)
+        {
+            int whos = UnityEngine.Random.Range(1, 3);
+            if (myCountries.Count >= 3)
+                AICountries.Add(Country);
+            else if (AICountries.Count >= 2)
+                myCountries.Add(Country);
+            else
+                if (whos == 1)
+                    myCountries.Add(Country);
+                
+                else if (whos == 2)
+                    AICountries.Add(Country);
+            return;
+        }
+    }
+
+}
---
+++ CountryClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountryClick : MonoBehaviour
+{
+    // meant to pop up when country is clicked on
+    public GameObject countryInfoCard;
+
+    // i havent implemented yet
+    public TextMeshProUGUI CountryName;
+    public TextMeshProUGUI IsMyCountry;
+
+    // on click function
+    public void Country1Clicked()
+    {
+        // if the GameObject 'Country1' is part of the 'myCountries' list then it is meant to change the IsMyCountry text to this
+        //but i get an alert in the console saying "\CountryClick.cs(19,45): error CS0103: The name 'Country1' does not exist in the current context"
+        if (ContryInfo.myCountries.Contains(Country1))
+        {
+            IsMyCountry.text = "Owned By: Me";
+            IsMyCountry.color = Color.green;
+        }
+        else
+        {
+            IsMyCountry.text = "Owned By: AI";
+            IsMyCountry.color = Color.red;
+        }
+
+        countryInfoCard.SetActive(!countryInfoCard.activeSelf);
+    }
+
+}
